b183fae335acb2b085bbd327d6ad1b51bd9cad89
Tayte Choudhury <t82020@outlook.com>
2025-01-30 19:53:56 -0600

    v3

--- Haskell Notes from Week 3.hs
+++ Haskell Notes from Week 3.hs
@@ -38,8 +38,50 @@ Pattern Matching - Many functions are defined using pattern matching on their ar
 
 Pattern can be a constant value or include one or more variable
 
+
+Wildcard Pattern Mathching, basically tells the compiler that the value isn't needed. 
+describeList :: [a] -> String
+describeList []     = "The list is empty."
+describeList (_:_)  = "The list is not empty."
+
+
+Ignored Function Parameters:
+constantFunction :: Int -> Int -> Int
+constantFunction x _ = x
 -}
 
 not :: Bool -> Bool
 not False = True 
 not True = False 
+
+
+{-
+Totality of Functions
+
+-Total Functions map every element in the function's domain to an element in its codomain
+-Partial function - maps zero or more elements if the functions domain to an element in its codomain and can leave some elements undefined
+-Haskell Functions CAN be partial:
+
+head (x:_) = x
+-}
+
+{-
+Lambda Expressions:
+- Functions can be consturcted without naming the functions by using lambda expressions
+-lambda is represented as the backslash \ 
+-Lambda Expressions can be used to give a formal meaning to functions defined using currying 
+- For example:
+add x y = x + y
+square x = x * x 
+
+which is same as 
+
+ add = \x -> (\y -> x + y)
+ square = \x -> x * x
+
+
+
+
+
+
+-}
